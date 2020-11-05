1ef196cff7df2b2440e51b4e811483510ba7533b
ajwkant <alexanderkant@hotmail.com>
2020-11-05 21:27:12 +0100

    first day on getnextline

--- get_next_line.c
+++ get_next_line.c
@@ -1,16 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   get_next_line.c                                    :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: akant <akant@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/11/05 13:38:38 by akant         #+#    #+#                 */
-/*   Updated: 2020/11/05 13:38:38 by akant         ########   odam.nl         */
+/*   Created: 2020/11/05 13:47:01 by akant         #+#    #+#                 */
+/*   Updated: 2020/11/05 15:51:45 by akant         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		main(int argc, char **argv)
+int		get_next_line(int fd, char **line)
 {
-	
+	if (!(fd || !line)
+	unsigned int bytes_read;
+
+	bytes_read = read(fd, buf, buffer_length);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,16 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   get_next_line.h                                    :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: akant <akant@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/11/05 13:38:38 by akant         #+#    #+#                 */
-/*   Updated: 2020/11/05 13:38:38 by akant         ########   odam.nl         */
+/*   Created: 2020/11/05 13:47:57 by akant         #+#    #+#                 */
+/*   Updated: 2020/11/05 13:52:20 by akant         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		main(int argc, char **argv)
-{
-	
-}
+#include <unistd.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+int		get_next_line(int fd, char **line);
+
+#endif
